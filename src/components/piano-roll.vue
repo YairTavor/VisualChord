d928a05fac807ce6e491452afa3a294d636ece39
Yair Tavor <yairtavor@gmail.com>
2019-11-17 13:43:29 +0100

    show chord notes on piano roll

--- src/components/piano-roll.vue
+++ src/components/piano-roll.vue
@@ -5,13 +5,13 @@
     <p v-if="!currentChord.root">To get starded, pick or create a chord in the Composer</p>
     <div v-else class="piano-roll__keyboard">
       <div class="piano-roll__note-list">
-        <div v-for="note in notes" :key="note" class="piano-roll__note">{{note}}</div>
+        <div v-for="note in notes" :key="note" :class="getNoteClass(note)">{{note}}</div>
       </div>
       <div class="piano-roll__sharp-list">
         <div
           v-for="(sharp, index) in sharps"
           :key="sharp+index"
-          :class="`piano-roll__sharp${sharp === 'SPACE' ? '--space' : ''}`"
+          :class="getNoteClass(sharp)"
         >{{sharp === 'SPACE' ? '' : sharp}}</div>
       </div>
     </div>
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import { notes, sharps } from "../lib/notes";
+import { notes, sharps, octaveNotes } from "../lib/notes";
 import "./piano-roll.scss";
 export default {
   props: {
@@ -27,6 +27,46 @@ export default {
   },
   data() {
     return { notes, sharps };
+  },
+  methods: {
+    getNotesInChord() {
+      let result = [];
+      if (this.currentChord.notes) {
+        result = this.currentChord.notes.map((chordNote, index) => {
+          let resultOcatve = "4";
+
+          if (index > 0) {
+            const root = this.currentChord.notes[0];
+            const startFromRoot = octaveNotes.indexOf(root);
+            const noteIndex = octaveNotes.indexOf(chordNote, startFromRoot);
+            if (noteIndex >= 12) {
+              resultOcatve = "5";
+            }
+          }
+
+          return chordNote + resultOcatve;
+        });
+      }
+      return result;
+    },
+    getNoteClass(note) {
+      let result = "";
+      const isSharp = note.indexOf("#") > -1;
+      const isSpace = note === "SPACE";
+      const isSelected = !isSpace && this.getNotesInChord().includes(note);
+
+      if (isSpace) {
+        result = "piano-roll__sharp--space";
+      } else if (!isSharp) {
+        result = "piano-roll__note";
+      } else {
+        result = "piano-roll__sharp";
+      }
+      if (isSelected) {
+        result += "--selected";
+      }
+      return result;
+    }
   }
 };
 </script>
--- src/lib/chord.js
+++ src/lib/chord.js
@@ -1,4 +1,5 @@
 import { roots, qualities, extensions } from "./enums";
+import { allNotes } from "./notes";
 
 const getKeyBySign = (sign, collection) => {
   const key = Object.keys(collection).find(
@@ -30,6 +31,83 @@ export default class Chord {
       this.extension.sign +
       (this.bass ? `/${this.bass.sign}` : "")
     );
+  }
+
+  get notes() {
+    let notes;
+    const root = this.root.sign.replace("♯", "#");
+    const rootIndex = allNotes.indexOf(root);
+    const minorSecondIndex = rootIndex + 1;
+    const majorSecondIndex = rootIndex + 2;
+    const minorThirdIndex = rootIndex + 3;
+    const majorThirdIndex = rootIndex + 4;
+    const perfectForthIndex = rootIndex + 5;
+    const tritoneIndex = rootIndex + 6; // augmented 4th or diminished 5th
+    const perfectFifthIndex = rootIndex + 7;
+    const minorSixthIndex = rootIndex + 8;
+    const majorSixthIndex = rootIndex + 9;
+    const minorSeventhIndex = rootIndex + 10;
+    const majorSeventhIndex = rootIndex + 11;
+    const nineIndex = rootIndex + 12 + 2;
+    const elevenIndex = rootIndex + 12 + 5;
+    const thirteenIndex = rootIndex + 12 + 9;
+
+    switch (this.quality) {
+      case qualities.MAJOR:
+        notes = [root, allNotes[majorThirdIndex], allNotes[perfectFifthIndex]];
+        break;
+      case qualities.MINOR:
+        notes = [root, allNotes[minorThirdIndex], allNotes[perfectFifthIndex]];
+        break;
+      case qualities.DIMINISHED:
+        notes = [root, allNotes[minorThirdIndex], allNotes[tritoneIndex]];
+        break;
+      case qualities.AUGMENTED:
+        notes = [root, allNotes[majorThirdIndex], allNotes[minorSixthIndex]];
+        break;
+      case qualities.SUSPENDED2:
+        notes = [root, allNotes[majorSecondIndex], allNotes[perfectFifthIndex]];
+        break;
+      case qualities.SUSPENDED4:
+        notes = [
+          root,
+          allNotes[perfectForthIndex],
+          allNotes[perfectFifthIndex]
+        ];
+        break;
+      case qualities.FIFTH:
+        notes = [root, allNotes[perfectFifthIndex]];
+        break;
+    }
+
+    switch (this.extension) {
+      case extensions.SIX:
+        notes.push(allNotes[majorSixthIndex]);
+        break;
+      case extensions.SEVEN:
+        notes.push(allNotes[minorSeventhIndex]);
+        break;
+      case extensions.MAJOR7:
+        notes.push(allNotes[majorSeventhIndex]);
+        break;
+      case extensions.ADD9:
+        notes.push(allNotes[nineIndex]);
+        break;
+      case extensions.NINE:
+        notes.push(allNotes[majorSeventhIndex]);
+        notes.push(allNotes[nineIndex]);
+        break;
+      case extensions.ELEVEN:
+        notes.push(allNotes[majorSeventhIndex]);
+        notes.push(allNotes[elevenIndex]);
+        break;
+      case extensions.THIRTEEN:
+        notes.push(allNotes[majorSeventhIndex]);
+        notes.push(allNotes[thirteenIndex]);
+        break;
+    }
+
+    return notes;
   }
 
   static parse(chord) {
